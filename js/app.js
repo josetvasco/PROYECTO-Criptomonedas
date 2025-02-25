3809db3302e178a9c73a65a01f6a15ea709c9424
josetvasco <torovascojosedaniel@gmail.com>
2025-02-24 20:40:29 -0500

    Cambio de fetch API a async await para la consulta de la API

--- js/app.js
+++ js/app.js
@@ -77,16 +77,24 @@ function mostrarAlerta( mensaje ) {
   }
 }
 
-function consultarAPI() {
+async function consultarAPI() {
   const { moneda, criptomoneda } = objBusqueda;
 
   const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
   mostrarSpinner();
 
-  fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(resultado => mostrarCotizacionHTML(resultado.DISPLAY[criptomoneda][moneda]))
+  // fetch(url)
+  //   .then(respuesta => respuesta.json())
+  //   .then(resultado => mostrarCotizacionHTML(resultado.DISPLAY[criptomoneda][moneda]))
+
+  try {
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    mostrarCotizacionHTML(resultado.DISPLAY[criptomoneda][moneda])
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function mostrarCotizacionHTML(cotizacion) {
